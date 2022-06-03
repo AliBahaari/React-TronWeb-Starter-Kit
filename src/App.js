223af233a28d44db05a873c1a3de5508dc4383f2
Ali Bahaari <alibahaari1997@gmail.com>
2022-06-04 02:07:01 +0430

    Account and Token Added along with Better Coding

--- src/App.js
+++ src/App.js
@@ -2,28 +2,57 @@ import {useEffect} from 'react';
 import './App.css';
 import TronWeb from 'tronweb';
 
+// const testOptions = {
+//   fullNode: "https://api.shasta.trongrid.io",
+//   solidityNode: "https://api.shasta.trongrid.io",
+//   eventServer: "https://api.shasta.trongrid.io"
+// }
+
 const mainOptions = {
-  fullNode: "https://api.shasta.trongrid.io",
-  solidityNode: "https://api.shasta.trongrid.io",
-  eventServer: "https://api.shasta.trongrid.io"
+  fullNode: "https://api.trongrid.io",
+  solidityNode: "https://api.trongrid.io",
+  eventServer: "https://api.trongrid.io"
 }
 
 const tronWeb = new TronWeb(mainOptions);
+tronWeb.setHeader({ "TRON-PRO-API-KEY": "" }); // Mainnet
+const walletAddress = "";
 
 function App() {
 
   const fetchBalance = async () => {
 
-    // tronWeb.setHeader({ "TRON-PRO-API-KEY": "" }); Mainnet
-    const balance = await tronWeb.trx.getBalance("");
-
+    const balance = await tronWeb.trx.getBalance(walletAddress);
     console.log(balance / 1000000);
+
+  }
+
+  const fetchAccout = async () => {
+
+    const account = await tronWeb.trx.getAccount(walletAddress);
+    console.log(account);
+
+  }
+
+  const fetchToken = async () => {
+
+    const trc20ContractAddress = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";
+
+    tronWeb.setAddress(trc20ContractAddress);
+    
+    const contract = await tronWeb.contract().at(trc20ContractAddress);
+    const contractBalance = await contract.balanceOf(walletAddress).call();
+    
+    console.log(contractBalance);
+
   }
 
   useEffect(() => {
     
     (async () => {
       await fetchBalance();
+      await fetchAccout();
+      await fetchToken();
     })();
 
   }, []);
